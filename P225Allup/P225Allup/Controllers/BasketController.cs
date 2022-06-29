008e5e032754a11d7508163109ecda4bc89b3e46
vusal-imanoff <vusalmi@code.edu.az>
2022-06-29 12:16:23 +0400

    done and can not without reload

---
+++ P225Allup/P225Allup/Controllers/BasketController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using P225Allup.DAL;
+using P225Allup.Models;
+using P225Allup.ViewModels.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P225Allup.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AddToBasket(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            List<BasketVM> basketVMs = null;
+
+            string cookie = HttpContext.Request.Cookies["basket"];
+
+            if (!string.IsNullOrEmpty(cookie))
+            {
+                basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+                if (basketVMs.Exists(p => p.Id == id))
+                {
+                    basketVMs.Find(p => p.Id == id).Count++;
+                }
+                else
+                {
+                   
+                    BasketVM basketVM = new BasketVM
+                    {
+                        Id = (int)id,
+                        Name = product.Title,
+                        Price = product.DiscountPrice>0 ? product.DiscountPrice : product.Price,
+                        Image=product.MainImage,
+                        Count = 1
+                    };
+                    basketVMs.Add(basketVM);
+                }
+            }
+            else
+            {
+                basketVMs = new List<BasketVM>();
+                BasketVM basketVM = new BasketVM
+                {
+                    Id = (int)id,
+                    Name=product.Title,
+                    Price = product.DiscountPrice > 0 ? product.DiscountPrice : product.Price,
+                    Image=product.MainImage,
+                    Count=1
+                };
+                basketVMs.Add(basketVM);
+            }
+            string item = JsonConvert.SerializeObject(basketVMs);
+            HttpContext.Response.Cookies.Append("basket", item);
+
+            return RedirectToAction("index", "home");
+        }
+        public IActionResult GetBasket()
+        {
+            string cookie = HttpContext.Request.Cookies["basket"];
+            List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+            return Json(basketVMs);
+        }
+
+        public IActionResult RemoveItem( int? id)
+        {
+            string cookie = HttpContext.Request.Cookies["basket"];
+            List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+            basketVMs.FindAll(x => x.Id != id);
+
+            string items = JsonConvert.SerializeObject(basketVMs.FindAll(x => x.Id != id));
+            HttpContext.Response.Cookies.Append("basket", items);
+
+            return RedirectToAction("index", "home");
+        }
+
+    }
+}
---
+++ P225Allup/P225Allup/Services/LayoutService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using P225Allup.DAL;
+using P225Allup.Interfaces;
+using P225Allup.ViewModels.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P225Allup.Services
+{
+    public class LayoutService : ILayoutService
+    {
+        private readonly AppDbContext _context;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LayoutService(AppDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public List<BasketVM> GetBasket()
+        {
+            string cookie = _httpContextAccessor.HttpContext.Request.Cookies["basket"];
+            List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+            return basketVMs;
+        }
+
+        public async Task<IDictionary<string,string>> GetSettingsAsync()
+        {
+            IDictionary<string, string> setting = await _context.Settings.ToDictionaryAsync(x => x.Key, x => x.Value);
+            return setting;
+        }
+        
+    }
+}
